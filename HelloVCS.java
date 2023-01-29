d6a150760da2e5866834c5494c35392c2721a1b7
Michael Bey <michael.bey@posteo.de>
2023-01-29 14:12:36 +0100

    Add calss Variable. Java Compendium Chapter 5

--- HelloVCS.java
+++ HelloVCS.java
@@ -2,6 +2,6 @@ public class HelloVCS {
 
     // Try to use all software together
     public static void main(String[] args) {
-        System.out.println("Helo VCS");
+        System.out.println("Hello VCS2");
     }
 }
---
+++ Variablen.java
@@ -0,0 +1,7 @@
+public class Variablen {
+	public static void main(String[] args) {
+		int mitarbeiternummer = 1423;
+		System.out.print("Mitarbeiternummer: ");
+		System.out.print(mitarbeiternummer);
+	}
+}
